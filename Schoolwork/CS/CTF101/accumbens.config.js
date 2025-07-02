e6d8a1ef9e8bd1b906a74fa2a48c517429d6c656
5dbwat4 <me@5dbwat4.top>
2025-07-03 06:42:54 +0800

    Update HPC101 lab1

--- Schoolwork/CS/CTF101/accumbens.config.js
+++ Schoolwork/CS/CTF101/accumbens.config.js
@@ -1,9 +1,5 @@
 export default {
     "dir":import.meta.url,
-    "path":{
-        "default":false,
-        "aliases":["Schoolwork/CS1264M","CS/CS1264M","CS/CTF101"], 
-    },
     "entries":"auto",
     "name":"CTF101",
     "show":false,
--- Schoolwork/Math/dir.js
+++ Schoolwork/Math/dir.js
@@ -1,6 +1,6 @@
 export default [
-    // "MA",
-    "MA2",
-    "LA",
-    "LA2",
+    // "MATH1163GH",
+    "MATH1164GH",
+    "MATH1261GH",
+    "MATH1262GH",
 ].map(name=> `./Math/${name}`);
