1c7b0449724e0ab49f77cfe7a43d206c765aa28d
5dbwat4 <me@5dbwat4.top>
2026-04-01 23:00:54 +0800

    push updates

---
+++ Schoolwork/25B/CS2053M/labs/accumbens.config.js
@@ -0,0 +1,14 @@
+export default {
+    "entries":[
+        {"file":"./lab1/report.mdx","path":"lab1"},
+        // {"file":"./lab2/report.mdx","path":"lab2"},
+        // {"file":"./lab3/report.mdx","path":"lab3"},
+        // {"file":"./lab4/report.mdx","path":"lab4"},
+        // {"file":"./lab5/report.mdx","path":"lab5"},
+        // {"file":"./lab6/report.mdx","path":"lab6"},
+        // {"file":"./lab7/report.mdx","path":"lab7"},
+        // {"file":"./lab-bonus/report.md","path":"lab-bonus"},
+        // {"file":"./project/report.mdx","path":"project"},
+    ],
+    "name":"SYS3-labs",
+}
