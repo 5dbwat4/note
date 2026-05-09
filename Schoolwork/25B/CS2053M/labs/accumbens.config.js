3c02411e38b72c51c132688dd171a0ea1241dc72
5dbwat4 <me@5dbwat4.top>
2026-05-10 04:29:11 +0800

    awa sys

--- Schoolwork/25B/CS2053M/labs/accumbens.config.js
+++ Schoolwork/25B/CS2053M/labs/accumbens.config.js
@@ -3,7 +3,7 @@ export default {
         {"file":"./lab1/report.mdx","path":"lab1"},
         {"file":"./lab2/report.mdx","path":"lab2"},
         {"file":"./lab3/report.mdx","path":"lab3"},
-        // {"file":"./lab4/report.mdx","path":"lab4"},
+        {"file":"./lab4/report.mdx","path":"lab4"},
         // {"file":"./lab5/report.mdx","path":"lab5"},
         // {"file":"./lab6/report.mdx","path":"lab6"},
         // {"file":"./lab7/report.mdx","path":"lab7"},
